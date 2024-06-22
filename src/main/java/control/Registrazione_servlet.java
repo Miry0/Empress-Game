89caaeb9fe0deac05b23c9fee9571087428293e7
Miry0 <m.desantis40@studenti.unisa.it>
2024-06-22 18:44:03 +0200

    servlet per gestire registrazione utenti

--- src/main/java/control/Registrazione_servlet.java
+++ src/main/java/control/Registrazione_servlet.java
@@ -16,7 +16,7 @@ import model.Utenti_bean;
 
 @WebServlet("/Registrazione_servlet/")
 public class Registrazione_servlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;;
     
     private Utenti_DAODataSource utenti; // DAO per l'interazione con il database degli utenti
     
