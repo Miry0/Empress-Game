7587bccff3e69e7ff180701c34a2eaa960d3c43b
Miry0 <m.desantis40@studenti.unisa.it>
2024-07-13 20:08:25 +0200

    carrello bean

--- src/main/java/model/Carrello_bean.java
+++ src/main/java/model/Carrello_bean.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.io.Serializable;
+import java.io.Serializable;
+import java.sql.Date;
 
 public class Carrello_bean implements Serializable {
 
@@ -10,9 +12,7 @@ public class Carrello_bean implements Serializable {
     private String nome_utente;
     private String metodo_pagamento;
     private Float totale;
-    private int g_ordine;
-    private int m_ordine;
-    private int a_ordine;
+    private Date data_ordine; // Campo per la data dell'ordine
     private byte[] immagine; // Aggiungi questa variabile per l'immagine
 
     // Costruttore
@@ -21,9 +21,7 @@ public class Carrello_bean implements Serializable {
         nome_utente = null;
         metodo_pagamento = null;
         totale = -1f;
-        g_ordine = -1;
-        m_ordine = -1;
-        a_ordine = -1;
+        data_ordine=new Date(System.currentTimeMillis()); // Inizializzazione con la data corrente
         immagine = null; // Inizializzazione dell'immagine
     }
 
@@ -69,34 +67,17 @@ public class Carrello_bean implements Serializable {
         this.totale = totale;
     }
 
-    public int get_g_ordine() {
-        return g_ordine;
+
+    public Date get_data_ordine() {
+        return data_ordine;
     }
 
-    public void set_g_ordine(int g_ordine) {
-        this.g_ordine = g_ordine;
-    }
-
-    public int get_m_ordine() {
-        return m_ordine;
-    }
-
-    public void set_m_ordine(int m_ordine) {
-        this.m_ordine = m_ordine;
-    }
-
-    public int get_a_ordine() {
-        return a_ordine;
-    }
-
-    public void set_a_ordine(int a_ordine) {
-        this.a_ordine = a_ordine;
+    public void set_data_ordine(Date data_ordine) {
+        this.data_ordine = data_ordine;
     }
 
     @Override
     public String toString() {
-        return "Carrello_bean [n_ordine=" + n_ordine + ", id_utente=" + nome_utente + ", metodo_pagamento="
-                + metodo_pagamento + ", totale=" + totale + ", g_ordine=" + g_ordine + ", m_ordine=" + m_ordine
-                + ", a_ordine=" + a_ordine + "]";
+        return "n_ordine=" + "nome_utente" + " metodo_pagamento" + " totale" + "data_ordine";
     }
 }
