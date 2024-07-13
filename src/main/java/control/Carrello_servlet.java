f13841d34f98c92fd7d8cca4dcaada1fd23bd36c
Miry0 <m.desantis40@studenti.unisa.it>
2024-07-13 20:18:11 +0200

    servlet del carrello

--- src/main/java/control/Carrello_servlet.java
+++ src/main/java/control/Carrello_servlet.java
@@ -1,7 +1,10 @@
 package control; 
 
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 
 import javax.servlet.RequestDispatcher;
@@ -11,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import model.Carrello_DAODataSource;
@@ -29,9 +33,11 @@ public class Carrello_servlet extends HttpServlet {
     }
 
  //gestiamo il carrello direttamente con il metodo dopost
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String action = request.getParameter("action");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	 DataSource ds=(DataSource) getServletContext().getAttribute("MyDataSource");  
+         carrelloDAO = new Carrello_DAODataSource(ds);
+         
+    	String action = request.getParameter("azione_carrello");
 
         if (action != null) {
             switch (action) {
@@ -68,14 +74,22 @@ public class Carrello_servlet extends HttpServlet {
     private void aggiungiElemento(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Recupera i parametri dalla richiesta
+    	// Parametri da richiesta HTTP
         int n_ordine = Integer.parseInt(request.getParameter("n_ordine"));
         String nome_utente = request.getParameter("nome_utente");
         String metodo_pagamento = request.getParameter("metodo_pagamento");
         float totale = Float.parseFloat(request.getParameter("totale"));
-        int g_ordine = Integer.parseInt(request.getParameter("g_ordine"));
-        int m_ordine = Integer.parseInt(request.getParameter("m_ordine"));
-        int a_ordine = Integer.parseInt(request.getParameter("a_ordine"));
-        byte[] immagine = request.getParameter("immagine").getBytes(); // Da verificare
+        String data_ordine_str = request.getParameter("data_ordine");
+        byte[] immagine = null; // Da implementare la gestione dell'immagine correttamente
+
+        // Conversione della data
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date data_ordine = null;
+        try {
+            data_ordine = (Date) sdf.parse(data_ordine_str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         // Crea un nuovo oggetto Carrello_bean
         Carrello_bean carrello = new Carrello_bean();
@@ -83,16 +97,17 @@ public class Carrello_servlet extends HttpServlet {
         carrello.set_nome_utente(nome_utente);
         carrello.set_metodo_pagamento(metodo_pagamento);
         carrello.set_totale(totale);
-        carrello.set_g_ordine(g_ordine);
-        carrello.set_m_ordine(m_ordine);
-        carrello.set_a_ordine(a_ordine);
+        carrello.set_data_ordine(data_ordine);
         carrello.setImmagine(immagine);
 
         // Salva il carrello nel database
         try {
             carrelloDAO.doSave(carrello);
             
-            request.setAttribute("carrello", carrello);
+            HttpSession session = request.getSession(); //recuperiamo la sessione e settiamo il nuovo oggetto da mostrare nel carrello
+            
+            session.setAttribute("carrello", carrello);
+            
             RequestDispatcher dispatcher = request.getRequestDispatcher("Gestione_carrello.jsp");
             dispatcher.forward(request, response);
         } catch (SQLException e) {
